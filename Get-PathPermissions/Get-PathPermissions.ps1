9e36012403118d7b3886f3fdb9e8bc97e42d31ea
jfrmilner <jfrmilner@googlemail.com>
2019-12-14 13:23:15 +0000

    Update Get-PathPermissions.ps1
    improve formatting and minor fixes

--- Get-PathPermissions/Get-PathPermissions.ps1
+++ Get-PathPermissions/Get-PathPermissions.ps1
@@ -1,18 +1,17 @@
 function Get-PathPermissions {
  
-    param ( [Parameter(Mandatory=$true)] [System.String]${Path} )
+    param ( [Parameter(Mandatory = $true)] [System.String]${Path} )
      
-        begin {
+    begin {
         $root = Get-Item $Path
-        ($root | get-acl).Access | Add-Member -MemberType NoteProperty -Name "Path" -Value $($root.fullname).ToString() -PassThru
-        }
-        process {
-        $containers = Get-ChildItem -path $Path -recurse | ? {$_.psIscontainer -eq $true}
-        if ($containers -eq $null) {break}
-            foreach ($container in $containers)
-            {
-            (Get-ACL $container.fullname).Access | ? { $_.IsInherited -eq $false } | Add-Member -MemberType NoteProperty -Name "Path" -Value $($container.fullname).ToString() -PassThru
-            }
+        ($root | Get-Acl).Access | Add-Member -MemberType NoteProperty -Name "Path" -Value $($root.fullname).ToString() -PassThru
+    }
+    process {
+        $containers = Get-ChildItem -LiteralPath $Path -Recurse | Where-Object { $_.psIscontainer -eq $true }
+        if ($null -eq $containers) { break }
+        foreach ($container in $containers) {
+            (Get-ACL $container.fullname).Access | Where-Object { $_.IsInherited -eq $false } | Add-Member -MemberType NoteProperty -Name "Path" -Value $($container.fullname).ToString() -PassThru
         }
     }
-    Get-PathPermissions $args[0]
+}
+Get-PathPermissions $args[0]
