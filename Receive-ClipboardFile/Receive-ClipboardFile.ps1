32cafa11e6566de18654587c5c539ef5699f9226
jfrmilner <jfrmilner@googlemail.com>
2022-03-24 19:20:20 +0000

    Initial commit
    Added Clipboard File transfer scripts

---
+++ Receive-ClipboardFile/Receive-ClipboardFile.ps1
@@ -0,0 +1,89 @@
+﻿
+function Receive-ClipboardFile {
+    <#
+    .SYNOPSIS
+    Receive a File using the Clipboard as transport.
+    .EXAMPLE
+    #Receive a File using the Clipboard
+    Receive-ClipboardFile -Path "C:\Support\ClipboardDemo"
+
+    Receive a File using the Clipboard
+    .NOTES
+    Author: jfrmilner
+    Web  : github.com/jfrmilner
+    File Name: Receive-ClipboardFile.ps1
+    Requires: Powershell V2
+    Legal: This script is provided "AS IS" with no warranties or guarantees, and confers no rights. You may use, modify, reproduce, and distribute this script file in any way provided that you agree to give the original author credit.
+    Version: v0.4 - 2022-03-24 - Github Release Version
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        $Path = $PWD.Path
+    )
+
+    begin {
+        Write-Host -Object "Receiver Start. Waiting for data Clipboard" -ForegroundColor Yellow
+    }#begin
+    process {
+        $cbArray = @()
+        Set-Clipboard "ready"
+        do {
+            $cb = Get-Clipboard -Raw
+            if ($cb -eq "ready") {
+                Write-Host "Waiting.."
+                Start-Sleep -Seconds 2
+            }
+
+            try {
+                $dataJSON = ConvertFrom-Json -InputObject $cb -ErrorAction SilentlyContinue
+
+                if ($dataJSON.FileName) {
+                    $cbArray += $dataJSON
+                    Write-Host $dataJSON.Part -ForegroundColor Cyan
+                    Set-Clipboard "ready"
+                }
+                else {
+                    #Write-Error -Message "Non-JSON"
+                }
+            }
+            catch {}
+        } until ($cb -eq "end")
+    }#process
+    end {
+
+        try {
+            #Remove Duplicates
+            $cbArray = $cbArray | Sort-Object -Property Position -Unique
+            #Basic Checks
+            $cbArray | Select-Object -Property * -ExcludeProperty Data
+            $parts = $cbArray[-1].Part -split "\/" | Select-Object -Skip 1
+            if ($cbArray.Count -ne $parts) {
+                Write-Error -Message "Missing Parts. Aborting"
+            }
+
+            #Part checksum validation with SHA256
+            foreach ($part in $cbArray) {
+                $part.Data | Out-File -LiteralPath "$env:TEMP\tmp.tmp" -Encoding utf8
+                $hashSHA256 = Get-FileHash -LiteralPath "$env:TEMP\tmp.tmp" -Algorithm SHA256
+                if ($part.HashSHA256 -ne $hashSHA256.Hash) {
+                    Write-Error -Message "Part Checksum Error. Aborting"
+                }
+            }
+
+            #Rebuild File
+            $base64New = New-Object -TypeName System.Text.StringBuilder
+            foreach ($part in $cbArray) {
+                $base64New.Append($part.Data)
+            }
+            #Save to Disk
+            Set-Content -Value $([System.Convert]::FromBase64String($base64New)) -Encoding Byte -Path $($path + '\' + $dataJSON.FileName)
+
+            #Create SHA256 Hash of saved file
+            $hash = Get-FileHash -LiteralPath $($path + '\' + $dataJSON.FileName)
+            Write-Host -Object $('Saving File ' + $path + '\' + $dataJSON.FileName + ' - Hash ' + $hash.Hash)
+        }
+        catch {}
+
+    }#end
+}
---
+++ Send-ClipboardFile/Send-ClipboardFile.ps1
@@ -0,0 +1,90 @@
+﻿function Send-ClipboardFile {
+    <#
+    .SYNOPSIS
+    Send a File using the Clipboard as transport.
+    .EXAMPLE
+    #Send a File Test.zip using the Clipboard at 150KB increments
+    Send-ClipboardFile -FilePath Test.zip -BufferSize 150KB
+
+    Send a File Test.zip using the Clipboard at 150KB increments
+    .NOTES
+    Author: jfrmilner
+    Web  : github.com/jfrmilner
+    File Name: Send-ClipboardFile.ps1
+    Requires: Powershell V2
+    Legal: This script is provided "AS IS" with no warranties or guarantees, and confers no rights. You may use, modify, reproduce, and distribute this script file in any way provided that you agree to give the original author credit.
+    Version: v0.4 - 2022-03-24 - Github Release Version
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateScript({ Test-Path $_ -PathType 'Leaf' })]
+        $FilePath,
+        [Int]
+        $BufferSize = 150KB
+    )
+
+    begin {
+        $filePath = Get-ChildItem -Path $FilePath
+        $hash = Get-FileHash -LiteralPath $FilePath
+        Write-Host -Object $('Sending File ' + $FilePath + ' - Size(MB):' + [Math]::Round(($filePath.Length / 1MB),2) + ' - Hash ' + $hash.Hash) -ForegroundColor Green
+    }#begin
+    process {
+        $file = [io.file]::OpenRead($filePath.FullName)
+        $buff = New-Object Byte[] $BufferSize
+        $parts = [math]::Ceiling($file.Length / 150KB)
+        $i = 1
+        do {
+            $part = ($i.ToString() + "/" + $parts.ToString())
+            Write-Host -Object $part
+            $i++
+            #do {
+            Start-Sleep -Seconds 2
+            Write-Host "Sleeping.."
+            # } until ((Get-Clipboard) -eq "ready")
+            $position = $file.Position
+            if (($file.Position + $buff.Length) -lt $file.Length) {
+                $read = $buff.Length
+                $file.Read($buff, 0, $read) | Out-Null
+                $data = [System.Convert]::ToBase64String($buff[0..($read - 1)])
+                $data | Out-File -LiteralPath "$env:TEMP\tmp.tmp" -Encoding utf8
+                $hashSHA256 = Get-FileHash -LiteralPath "$env:TEMP\tmp.tmp" -Algorithm SHA256
+                $dataHT = @{
+                    "FileName"   = $filePath.Name
+                    "Position"   = $position
+                    "Part"       = $part
+                    "HashSHA256" = $hashSHA256.Hash
+                    "Data"       = $data
+                }
+            }
+            else {
+                $read = $file.Length - $file.Position
+                $file.Read($buff, 0, $read) | Out-Null
+                $data = [System.Convert]::ToBase64String($buff[0..($read - 1)])
+                $data | Out-File -LiteralPath "$env:TEMP\tmp.tmp" -Encoding utf8
+                $hashSHA256 = Get-FileHash -LiteralPath "$env:TEMP\tmp.tmp" -Algorithm SHA256
+                $dataHT = @{
+                    "FileName"   = $filePath.Name
+                    "Position"   = $position
+                    "Part"       = $part
+                    "HashSHA256" = $hashSHA256.Hash
+                    "Data"       = $data
+                }
+            }
+            $dataJSON = $dataHT | ConvertTo-Json -Compress
+            Set-Clipboard $dataJSON
+
+        } until ($file.Position -eq $file.Length)
+        $file.Close()
+
+    }#process
+    end {
+        #do {
+        Start-Sleep -Seconds 3
+        Write-Host "Sleeping.."
+        #} until ((Get-Clipboard) -eq "ready")
+        Set-Clipboard -Value "end"
+        Write-Host -Object "Send Complete"
+    }#end
+}
+
